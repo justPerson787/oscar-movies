494228aacdd6525e20928cc9077feb4e25cffb31
N.U <n.n.ushakova@gmail.com>
2021-01-24 23:56:59 -0500

    Pass data to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Card = () => {
-    return <h2>movie #1</h2>;
+const Card = ({ movie }) => {
+    return (
+        <div>
+            <h2>{`#${movie.nomination} - ${movie.title} (${movie.year})`}</h2>
+            <img src={movie.img.src} alt={movie.img.alt} width='200' />
+            <p>{`Nomination: ${movie.nomination}`}</p>
+        </div>
+    );
 };
 
 export default Card;
